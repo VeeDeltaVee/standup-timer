62748998ea31c8301684e9f212b3d90a843631f1
Versha Dhankar <vdhankar@microsoft.com>
2022-05-19 10:18:00 -0700

    Finished dev for clock component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Text } from '@fluentui/react';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root') as HTMLElement;
 
+export type ClockProps = { };
+
+export const Clock = ({}): JSX.Element => {
+  const [startTime, _] = useState<Date>(new Date());
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setElapsedTime((new Date()).getTime() - startTime.getTime());
+    }, 1000);
+  });
+
+  let timeToDisplay = new Date(elapsedTime);
+  let timeToDisplayString = `${timeToDisplay.getMinutes()}:${timeToDisplay.getSeconds()}`;
+
+  return (
+    <Text> {timeToDisplayString} </Text>
+  );
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <Text>Testing!</Text>
+    <Clock />
   </React.StrictMode>
   , root);
 
