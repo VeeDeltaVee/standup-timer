16cb8540871aabd644f45c7bece2eb29d44e5d18
Versha Dhankar <vdhankar@microsoft.com>
2022-05-19 12:04:54 -0700

    Got a simple timer to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,106 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { Text } from '@fluentui/react';
-import reportWebVitals from './reportWebVitals';
+import { Text, Stack, getTheme, createTheme, ITheme, Label, ProgressIndicator, DefaultButton } from '@fluentui/react';
+import { useInterval } from 'react-interval-hook';
 
-const root = document.getElementById('root') as HTMLElement;
+const myTheme = createTheme({
+  palette: {
+    themePrimary: '#0078d4',
+    themeLighterAlt: '#eff6fc',
+    themeLighter: '#deecf9',
+    themeLight: '#c7e0f4',
+    themeTertiary: '#71afe5',
+    themeSecondary: '#2b88d8',
+    themeDarkAlt: '#106ebe',
+    themeDark: '#005a9e',
+    themeDarker: '#004578',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralSecondaryAlt: '#8a8886',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff',
+  }});
 
-export type ClockProps = { };
+const theme: ITheme = getTheme();
 
-export const Clock = ({}): JSX.Element => {
-  const [startTime, _] = useState<Date>(new Date());
-  const [elapsedTime, setElapsedTime] = useState(0);
+export type ClockProps = { displayedTime: number, labelText: string};
 
-  useEffect(() => {
-    setTimeout(() => {
-      setElapsedTime((new Date()).getTime() - startTime.getTime());
-    }, 1000);
-  });
-
-  let timeToDisplay = new Date(elapsedTime);
-  let timeToDisplayString = `${timeToDisplay.getMinutes()}:${timeToDisplay.getSeconds()}`;
+export const Clock = ({displayedTime, labelText}: ClockProps): JSX.Element => {
+  let displayedTimeAsDate = new Date(displayedTime);
+  let displayedTimeString = `${displayedTimeAsDate.getMinutes()} : ${displayedTimeAsDate.getSeconds()}`;
 
   return (
-    <Text> {timeToDisplayString} </Text>
+    <Stack>
+      <Label>{labelText}</Label>
+      <Text variant='xLarge' > {displayedTimeString} </Text>
+    </Stack>
   );
 }
 
+export type ScrumTimerProps = {
+};
 
+export const ScrumTimer = (): JSX.Element => {
+  const [teamStartTime, setTeamStartTime] = useState<Date>();
+  const [teamElapsedTime, setTeamElapsedTime] = useState(0);
+
+  const [secondsPerSpeaker, setSecondsPerSpeaker] = useState(60);
+
+  const [currentSpeakerStartTime, setCurrentSpeakerStartTime] = useState<Date>();
+  const [currentSpeakerElapsedTime, setCurrentSpeakerElapsedTime] = useState(0);
+  const [currentSpeakerPercentTimeUsed, setCurrentSpeakerPercentTimeUsed] = useState(0);
+
+  const [isScrumRunning, setIsScrumRunning] = useState(false);
+  const [speakerNumber, setSpeakerNumber] = useState(0);
+
+  useEffect(() => {
+    if (isScrumRunning) {
+      setTeamStartTime(new Date());
+      setCurrentSpeakerStartTime(new Date());
+    }
+  }, [isScrumRunning]);
+
+  useInterval(() => {
+    let currentTime = new Date();
+
+    if (teamStartTime && currentSpeakerStartTime && isScrumRunning) {
+      setTeamElapsedTime(currentTime.getTime() - teamStartTime.getTime());
+      setCurrentSpeakerElapsedTime(currentTime.getTime() - currentSpeakerStartTime.getTime());
+      setCurrentSpeakerPercentTimeUsed(currentSpeakerElapsedTime / (secondsPerSpeaker * 1000));
+    }
+  }, 100);
+
+  return (
+    <Stack horizontal styles={{root: { padding: '20px', width: '500px' }}}>
+      <Stack tokens={{childrenGap: 10, padding: 20}} styles={{root: { boxShadow: theme.effects.elevation4 }}}>
+        {isScrumRunning &&
+          <Stack>
+            <ProgressIndicator label={`Speaker ${speakerNumber + 1} Time Used`} percentComplete={currentSpeakerPercentTimeUsed}/>
+            <Clock labelText="Team Time" displayedTime={teamElapsedTime} />
+          </Stack>
+        }
+
+        <Stack horizontal tokens={{childrenGap: 10, padding: 10}}>
+          <DefaultButton text={isScrumRunning ? "Stop Scrum" : "Start Scrum"} onClick={() => {if (!isScrumRunning) { setSpeakerNumber(0) } ; setIsScrumRunning(!isScrumRunning)}} />
+          {isScrumRunning && <DefaultButton text="Next Speaker" onClick={() => {setSpeakerNumber(speakerNumber + 1); setCurrentSpeakerStartTime(new Date())}} />}
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+}
+
+const root = document.getElementById('root') as HTMLElement;
 ReactDOM.render(
   <React.StrictMode>
-    <Clock />
+    <ScrumTimer />
   </React.StrictMode>
   , root);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
